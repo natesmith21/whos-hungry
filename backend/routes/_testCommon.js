81aa3d407cd4fe4c87e9664a295d9f7322c165e7
Nate Smith <158646537+natesmith21@users.noreply.github.com>
2025-03-26 10:50:11 -0700

    set up simple beginning testing

---
+++ backend/routes/_testCommon.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const db = require("../db.js");
+const User = require("../models/user");
+const { createToken } = require("../helpers/tokens");
+
+
+async function commonBeforeAll() {
+  // noinspection SqlWithoutWhere
+  await db.query("DELETE FROM users");
+
+  await User.register({
+    username: "u1",
+    firstName: "U1F",
+    lastName: "U1L",
+    email: "user1@user.com",
+    password: "password1",
+    isAdmin: false,
+  });
+  await User.register({
+    username: "u2",
+    firstName: "U2F",
+    lastName: "U2L",
+    email: "user2@user.com",
+    password: "password2",
+    isAdmin: false,
+  });
+  await User.register({
+    username: "u3",
+    firstName: "U3F",
+    lastName: "U3L",
+    email: "user3@user.com",
+    password: "password3",
+    isAdmin: false,
+  });
+}
+
+async function commonBeforeEach() {
+  await db.query("BEGIN");
+}
+
+async function commonAfterEach() {
+  await db.query("ROLLBACK");
+}
+
+async function commonAfterAll() {
+    await db.end();
+  }
+
+  module.exports = { commonBeforeAll, commonBeforeEach, commonAfterAll, commonAfterEach }
