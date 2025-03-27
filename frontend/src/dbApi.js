5f3630ad636b993e0e68a345226ed865140f9865
Nate Smith <158646537+natesmith21@users.noreply.github.com>
2025-03-27 11:57:42 -0700

    connected frontend

--- frontend/src/dbApi.js
+++ frontend/src/dbApi.js
@@ -11,7 +11,7 @@ class dbApi {
         //there are multiple ways to pass an authorization token, this is how you pass it in the header.
         //this has been provided to show you another way to pass the token. you are only expected to read this code for this project.
         const url = `${BASE_URL}/${endpoint}`;
-        const headers = { Authorization: `Bearer ${JoblyApi.token}` };
+        const headers = { Authorization: `Bearer ${dbApi.token}` };
         const params = (method === "get")
             ? data
             : {};
