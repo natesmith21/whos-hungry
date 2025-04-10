584ddae31baedf39ceba8cc4163249c5b72e1dca
Nate Smith <158646537+natesmith21@users.noreply.github.com>
2025-04-10 10:28:57 -0700

    cannot get jest configured to work

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -2,6 +2,7 @@ import React from "react";
 import { stripHtml } from "string-strip-html";
 
 
+// this pacakge + function seems to work well enough for right now, but I wish the summary section looked better. I was fearful of using 'dangerouslySetInnerHTML', because i'm not sure I fully trust all the info from the API
 export const cleanHTML = (dirtyHTML) => {
    let cleaned =  stripHtml(dirtyHTML, {
         skipHtmlDecoding: true,
