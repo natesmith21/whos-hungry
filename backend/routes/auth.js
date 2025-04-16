56e864ae2b654611a1e2f7e7661316162a0336f9
Nate Smith <158646537+natesmith21@users.noreply.github.com>
2025-04-16 14:58:06 -0700

    console.log in auth

--- backend/routes/auth.js
+++ backend/routes/auth.js
@@ -20,6 +20,7 @@ const { BadRequestError } = require("../expressError");
  */
 
 router.post("/token", async function (req, res, next) {
+  console.log('auth/token', req.headers, req.body)
   try {
     const validator = jsonschema.validate(req.body, userAuthSchema);
     if (!validator.valid) {
